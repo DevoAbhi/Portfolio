0e355a1da847890cff3f8370f1485333f6a0fcc7
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-27 20:47:30 +0530

    add projects component in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Projects from './components/Projects/Projects';
 
 const App = () => {
   return (
     <>
     <Navbar></Navbar>
     <Home></Home>
+    <Projects></Projects>
     </>
   )
 }
