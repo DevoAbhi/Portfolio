acf04d8d2199ba166732043a14a74ffcaae7576a
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-27 20:46:26 +0530

    create my projects in the form of json

---
+++ src/components/Projects/myProjects.js
@@ -0,0 +1,47 @@
+export const projects = [
+    {
+        title: "Trump Card Game",
+        desc: "Remember your childhood when you used to collect trump cards and play with your friends? Now experience the same fun!",
+        website: "https://github.com/DevoAbhi/TrumpCard"
+    },
+    {
+        title: "Fileshare Essentials",
+        desc: "A single page application for you to send your files.",
+        website: "https://github.com/DevoAbhi/FileShare-Frontend"
+    },
+    {
+        title: "BookYourMark",
+        desc: "This is a interactive web app that helps the user to personalize their bookmarks and access all of their useful tabs all at one place.",
+        website: "https://github.com/DevoAbhi/BookYourMark"
+    },
+    {
+        title: "Color Mate",
+        desc: "This is a module using which you can find the dominant color or average color from images.",
+        website: "https://github.com/DevoAbhi/ColorMate"
+    },
+    {
+        title: "WhatsSend",
+        desc: "Send your loved ones pre scheduled messages.",
+        website: "https://github.com/DevoAbhi/WhatsSend-Frontend"
+    },
+    {
+        title: "Posterly",
+        desc: "Posterly is a post app where you can save your post and read them whenever you want.",
+        website: "https://github.com/DevoAbhi/Posterly"
+    },
+    {
+        title: "BooksPoint",
+        desc: "A library management website",
+        website: "https://github.com/DevoAbhi/Books-Point"
+    },
+    {
+        title: "YelpPoint",
+        desc: "This is a Camping Ground Website which helps us find camping grounds for picnics and night outs.",
+        website: "https://github.com/DevoAbhi/YelpCamp"
+    },
+    {
+        title: "Zootop",
+        desc: "Meet with your friends online.",
+        website: "https://github.com/DevoAbhi/ZOOTOP"
+    },
+]
