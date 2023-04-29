7b50f7405962d56147680c74215763af1411bdca
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-30 01:42:11 +0530

    create myExperiences and relocate myProjects

---
+++ src/helper/myExperiences.js
@@ -0,0 +1,38 @@
+export const experiences = [
+    {
+        "company_name": "Eternalight Infotech",
+        "position": "Full Stack Developer Intern",
+        "start_date": "June 2022",
+        "end_date": "Present",
+        "responsibilities": [
+            "Used AWS tools like S3, Cloudfront and Route 53 to deploy the frontend.",
+            "Developed 21 web scraping scripts.",
+            "Built a script to get dominant color from 100s logo images.",
+            "Responsible for developing POCs for new ideas."
+        ]
+    },
+    {
+        "company_name": "Vanna",
+        "position": "Backend Developer Intern",
+        "start_date": "Apr 2021",
+        "end_date": "Oct 2021",
+        "responsibilities": [
+            "Initiated Learnreel App and achieved 500+ downloads within 1 year period with 4.9 rating.",
+            "Built Facebook, Twitter and Linkedin user logins from scratch.",
+            "Minimized huge file processing time using AWS SQS batch requests.",
+            "Built app notification service using Amazon SNS and Firebase."
+        ]
+    },
+    {
+        "company_name": "Canvasbird",
+        "position": "Contributor",
+        "start_date": "Nov 2020",
+        "end_date": "Jan 2021",
+        "responsibilities": [
+            "Built a hide password feature.",
+            "Performed bug fixes and feature additions.",
+            "Built a home page website which increased user interaction by 60%."
+        ]
+    }
+]
+
