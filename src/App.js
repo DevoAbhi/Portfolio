f012c9de22121a5d7906b5be4f7b4f1670bf0688
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-30 01:46:26 +0530

    add all the components in app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import Experience from './components/Experience/Experience';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
-
+import ContactMe from './components/ContactMe/ContactMe';
 const App = () => {
   return (
     <>
     <Navbar></Navbar>
     <Home></Home>
     <Projects></Projects>
+    <Experience></Experience>
+    <ContactMe></ContactMe>
     </>
   )
 }
