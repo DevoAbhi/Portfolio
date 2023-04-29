cef83efd441e9526ed6411b704af9f43ce806c2c
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-30 01:43:42 +0530

    make the slides per view responsive

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -5,16 +5,39 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import { projects } from './myProjects';
-import Card from './Card';
+import { projects } from '../../helper/myProjects';
+import Card from '../Card/Card';
+import { useState,useEffect } from 'react';
 import './ProjectItem.css'
 
 const ProjectItem = () => {
+
+    const [slidesPerView, setSlidesPerView] = useState(3);
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth >= 768) {
+                setSlidesPerView(3);
+            } else if (window.innerWidth >= 576) {
+                setSlidesPerView(2);
+            } else {
+                setSlidesPerView(1);
+            }
+        }
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={20}
-            slidesPerView={3}
+            slidesPerView={slidesPerView}
             navigation
             pagination={{ clickable: true }}
             scrollbar={{ draggable: true }}
